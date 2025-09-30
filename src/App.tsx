deadb1609e68c4a257890070e70ef59d9588a574
LF236 <fernandorodriguez236@gmail.com>
2025-09-30 13:54:27 -0600

    Start front project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+	return (
+		<>
+			<div>
+				<p>LF236 - Full Stack Web Developer</p>
+				<small>Build bills project</small>
+			</div>
+		</>
+	)
+}
+
+export default App
