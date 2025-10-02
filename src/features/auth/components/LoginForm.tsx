c693b94b77c4dce02c573f3ad7345b3da56e3672
LF236 <fernandorodriguez236@gmail.com>
2025-10-02 16:59:32 -0600

    Adding formik

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,29 +1,60 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Button } from '../../common/components/Button';
 import { TextField } from '../../common/components/Fields';
+import * as Yup from 'yup';
 
 export const LoginForm = () => {
-    return (
-        <form>
-            <div className="space-y-6">
-                <TextField
-                    label="Email address"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                />
-                <TextField
-                    label="Password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                />
+	const validationSchema = Yup.object({
+		email: Yup.string().email('Invalid email adress').required('Email is required'),
+		password: Yup.string().required('Password is required'),
+	});
 
-                <Button type="submit" color='cyan' className="w-full mt-8">
-                    Sign in to account
-                </Button>
-            </div>
-        </form>
-    );
+	const handleSubmit = (values: { email: string; password: string }) => {
+		console.log(values);
+	}
+
+	return (
+		<Formik
+			initialValues={{ email: '', password: '' }}
+			validationSchema={validationSchema}
+			onSubmit={handleSubmit}
+		>
+			<Form>
+				<div className="space-y-6">
+					<Field
+						label="Email address"
+						name="email"
+						type="email"
+						autoComplete="email"
+						required
+						component={TextField}
+
+					/>
+					<ErrorMessage
+						name="email"
+						render={(msg) => (
+							<p className="mt-1 text-sm text-red-600 bg-red-50 border border-red-200 px-3 py-1 rounded-md">
+								{msg}
+							</p>
+						)}
+					/>
+
+					<Field
+						label="Password"
+						name="password"
+						type="password"
+						autoComplete="current-password"
+						required
+						component={TextField}
+					/>
+
+					<ErrorMessage name="password" component="div" className="text-red-500 text-sm" />
+
+					<Button type="submit" color='cyan' className="w-full mt-8">
+						Sign in to account
+					</Button>
+				</div>
+			</Form>
+		</Formik>
+	);
 }
